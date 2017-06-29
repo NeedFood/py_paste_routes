8f0c0fe3b1ba4f025755b6f879f72e1167066ef7
NeedFood <992581953@qq.com>
2017-06-29 19:26:26 +0800

    a common wsgi

--- v1/route.py
+++ v1/route.py
@@ -1,11 +1,11 @@
-import wsgi1
+import wsgi
 
 
 class ControllerTest(object):
     def __init__(self):
         print "ControllerTest!!!"
 
-    def test(self,req):
+    def test(self, req):
         print "req", req
         return {
             'name': 'test',
@@ -13,14 +13,14 @@ class ControllerTest(object):
         }
 
 
-class MyRouterApp(wsgi1.Router):
+class MyRouterApp(wsgi.Router):
     '''
     app
     '''
     def __init__(self, mapper):
         controller = ControllerTest()
         mapper.connect('/test',
-                       controller=wsgi1.Resource(controller),
+                       controller=wsgi.Resource(controller),
                        action='test',
                        conditions={'method': ['GET']})
         super(MyRouterApp, self).__init__(mapper)
